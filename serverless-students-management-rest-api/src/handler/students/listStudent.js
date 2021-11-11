79a9d0999899af2d5747a92d11c8eb4157805b67
thongdb <dinhbachthong@gmail.com>
2021-11-11 16:56:49 +0700

    connect redis client

--- serverless-students-management-rest-api/src/handler/students/listStudent.js
+++ serverless-students-management-rest-api/src/handler/students/listStudent.js
@@ -6,11 +6,33 @@
 'use strict'
 
 const AWS = require('aws-sdk');
-
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const redis = require('redis');
+const redisUrl = 'redis://3.0.19.255:7001';
+// Error here!
+// const client = redis.createClient(redisUrl);
 
 module.exports.listStudent = (event, context, callback) => {
 
+    // Check if we have value for students in redis
+    // let students = null
+    // client.hget("students", "all", (err, val) => { students = val });
+    // console.log("STUDENTS", students);
+
+    // If we do, return that
+    // if (students) {
+    //     const doc = JSON.parse(students);
+
+    //     console.log("DOC: ", doc);
+    //     console.log("READ STUDENTS FROM REDIS");
+
+    //     // return Array.isArray(doc) 
+    //     // ? doc.map( d => this.model(d))
+    //     // : new this.model(doc)
+    // }
+ 
+    // Otherwise, exec the query
     const params = {
         TableName: 'students'
     };
@@ -26,6 +48,10 @@ module.exports.listStudent = (event, context, callback) => {
             statusCode: 200,
             body: JSON.stringify(data.Items)
         };
+
+        // // Store the result in redis
+        // console.log("STORE DATA TO REDIS");
+        // console.log("RESPONSE: ", response.body);
 
         callback(null, response);
     });
