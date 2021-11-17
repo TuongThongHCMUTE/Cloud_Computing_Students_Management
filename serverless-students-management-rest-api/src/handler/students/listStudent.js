5992f708e5102b0e4cdb9d23fa5f4ca0d7a6dec3
thongdb <dinhbachthong@gmail.com>
2021-11-18 02:00:34 +0700

    Cache list students with redis

--- serverless-students-management-rest-api/src/handler/students/listStudent.js
+++ serverless-students-management-rest-api/src/handler/students/listStudent.js
@@ -10,49 +10,59 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const redis = require('redis');
 const redisUrl = 'redis://3.0.19.255:7001';
-// Error here!
-// const client = redis.createClient(redisUrl);
+const redisClient = redis.createClient(redisUrl);
+
+// const util = require('util');
+// redisClient.hget = util.promisify(redisClient.hget);
 
 module.exports.listStudent = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
 
-    // Check if we have value for students in redis
-    // let students = null
-    // client.hget("students", "all", (err, val) => { students = val });
-    // console.log("STUDENTS", students);
-
-    // If we do, return that
-    // if (students) {
-    //     const doc = JSON.parse(students);
-
-    //     console.log("DOC: ", doc);
-    //     console.log("READ STUDENTS FROM REDIS");
-
-    //     // return Array.isArray(doc) 
-    //     // ? doc.map( d => this.model(d))
-    //     // : new this.model(doc)
-    // }
- 
-    // Otherwise, exec the query
-    const params = {
-        TableName: 'students'
-    };
-
-    dynamoDb.scan(params, (error, data) => {
-        if(error) {
-            console.error(error);
-            callback(new Error(error));
-            return;
+    // Check if we have value for list students in redis
+    redisClient.hget("students", "all", (err, val) => {
+        if (val) {
+            console.log("CACHE HIT. READ STUDENTS FROM REDIS");
+            console.log("STUDENTS: ", val);
+            
+            const response = {
+                statusCode: 200,
+                body: val
+            };
+    
+            callback(null, response);
+        } else {
+            // Otherwise, exec the query and store result into redis 
+            const params = {
+                TableName: 'students'
+            };
+    
+            dynamoDb.scan(params, (error, data) => {
+                if(error) {
+                    console.error(error);
+                    callback(new Error(error));
+                    return;
+                }
+    
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify(data.Items)
+                };
+    
+                // Store the result in redis
+                console.log("CACHE MISS. READ STUDENTS FROM DYNAMODB AND STORE TO REDIS");
+                redisClient.hset("students", "all", response.body, (err, val) => {
+                    if(err) {
+                        console.log("ERR: ",err);
+                    }
+                    console.log("VAL: ", val);
+                });
+    
+                // const timestamp = 300;
+                // redisClient.expire("students", timestamp)
+                callback(null, response);
+            });
         }
-
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(data.Items)
-        };
-
-        // // Store the result in redis
-        // console.log("STORE DATA TO REDIS");
-        // console.log("RESPONSE: ", response.body);
-
-        callback(null, response);
     });
+
+
 }
